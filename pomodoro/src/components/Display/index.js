555d92d7d559983662475afe113df82f96bc21b1
FelipeTiberio <felipetiberio@yahoo.com.br>
2021-01-23 20:32:24 -0300

    Terminei de implementar o countdown para um seção de tamanho n.

--- pomodoro/src/components/Display/index.js
+++ pomodoro/src/components/Display/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Display = (props) =>{
     return(
         <section id ='count-container'>
-                { props.minutos}:{props.segundos}
+                { props.minutos < 10 ? "0" + props.minutos : props.minutos}
+                :
+                { props.segundos < 10 ? "0" + props.segundos : props.segundos}
         </section>
     )
 };
--- pomodoro/src/pages/Landing/index.js
+++ pomodoro/src/pages/Landing/index.js
@@ -1,21 +1,34 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Display from '../../components/Display';
 import PlusMinusButtons from '../../components/PlusMinusButtons';
 import SimpleButton from '../../components/SimpleButton';
 import './styles.css'
 
+const STANDARD_SESSION_DURATION = 1500;
+const STANDARD_BREAK_DURATION = 300;
+
 function Landing(){
     
-    const [ durationSession, setDurantionSession ] = useState(1500);
-    const [ durationBreak, setDurantionBreak ]= useState(300);
-    const [ timeLeft , setTimeLeft ] = useState(1500);
-    const [ start_turn, setStarTurn] = useState(false);
-    const [ countDown, setCountdown] = useState({minutes:60, seconds: 60})
+    const [ durationSession, setDurantionSession ] = useState( STANDARD_SESSION_DURATION);
+    const [ durationBreak, setDurantionBreak ]= useState(STANDARD_BREAK_DURATION);
+    const [ isCounting, setIsCounting] = useState(false);
+    const [ countdown, setCountdown] = useState( STANDARD_SESSION_DURATION);
 
-    const convertSecondsToMinutes = (seconds) => seconds / 60;
+    let intervalo = useRef();
+
+    useEffect( () => {
+        /** Run in the backgroud  e count the seconds 
+         * using the value that is in countdown.
+         */
+        intervalo.current = setTimeout( () =>{
+          if( isCounting ){
+            setCountdown( () => ( countdown - 1))
+          }
+        }, 1000)
+      })
 
     const addBreakTime = () => { 
-            setDurantionBreak( (durationBreak) =>  durationBreak + 60 )
+            setDurantionBreak( (durationBreak) =>  durationBreak + 60 );
         };
 
     const subBreakTime = () => { 
@@ -30,14 +43,21 @@ function Landing(){
         setDurantionSession( (durationSession) =>  durationSession - 60 )
         };
 
-    const zerar =  () => {
-       
-    }
+        const zerar = async () =>{
+            await setIsCounting(false);
+            await clearTimeout(intervalo.current);
+            await setCountdown( durationSession );
+          } 
 
     const start = () =>{
-       
+       setIsCounting( () => !isCounting );
+       setCountdown( () =>  durationSession );
     }
 
+    const  pause = () =>{
+        clearTimeout(intervalo.current);
+        setIsCounting( ()=> !isCounting );
+  }
     return(
         <main>
             <section id = 'top-buttons'> 
@@ -45,35 +65,27 @@ function Landing(){
                     onClickAddMinute  = {addBreakTime}
                     onClickSubMinute  = {subBreakTime}
                     className = "break-buttons" 
-                    number={ convertSecondsToMinutes(durationBreak)} />
+                    number={ Math.floor( ( durationBreak )/60 )%60 } />
 
                 <PlusMinusButtons 
                     onClickAddMinute = {addSessionTime}
                     onClickSubMinute = {subSessionTime}
                     className = "timelenght-buttons" 
-                    number={ convertSecondsToMinutes(durationSession)} />
+                    number = { Math.floor( ( durationSession )/60 )%60 } />
 
             </section>
 
                 <Display  
-                    minutos={ 
-                        start_turn === false ?
-                        durationSession/60  : 
-                        countDown.minutes/60
-                    } 
-                    segundos={
-                        start_turn === false ?
-                        "00":
-                        countDown.seconds/60
-                    }/>
+                    minutos  = { !isCounting ? Math.floor( (durationSession)/60) % 60 : Math.floor( (countdown)/60 )%60 } 
+                    segundos = { Math.floor( (countdown) % 60 ) }/>
 
             <section id = 'start-rest-container'>
                 <SimpleButton 
                     label={ 
-                        start_turn === false ?
+                        isCounting === false ?
                         "Start" : "Pause"
                     }
-                    onClick={start}
+                    onClick= { isCounting ? pause : start}
                     />
                 <SimpleButton label='Reset' onClick={zerar} />
             </section>
